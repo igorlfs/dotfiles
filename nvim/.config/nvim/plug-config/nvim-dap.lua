681958946164aef5a9f0b6901ea4f0a685af93b5
igorlfs <igorlfs@ufmg.br>
2021-07-12 16:50:58 -0300

    [nvim] DAP: function to pick a process

--- nvim/.config/nvim/plug-config/nvim-dap.lua
+++ nvim/.config/nvim/plug-config/nvim-dap.lua
@@ -1,3 +1,49 @@
+-- function to choose a process
+local dap = require('dap')
+local function pick_process()
+  local output = vim.fn.system({'ps', 'a'})
+  local lines = vim.split(output, '\n')
+  local procs = {}
+  for _, line in pairs(lines) do
+    -- output format
+    --    " 107021 pts/4    Ss     0:00 /bin/zsh <args>"
+    local parts = vim.fn.split(vim.fn.trim(line), ' \\+')
+    local pid = parts[1]
+    local name = table.concat({unpack(parts, 5)}, ' ')
+    if pid and pid ~= 'PID' then
+      pid = tonumber(pid)
+      if pid ~= vim.fn.getpid() then
+        table.insert(procs, { pid = tonumber(pid), name = name })
+      end
+    end
+  end
+  local choices = {'Select process'}
+  for i, proc in ipairs(procs) do
+    table.insert(choices, string.format("%d: pid=%d name=%s", i, proc.pid, proc.name))
+  end
+  local choice = vim.fn.inputlist(choices)
+  if choice < 1 or choice > #procs then
+    return nil
+  end
+  return procs[choice].pid
+end
+-- Javascript
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/prog/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+dap.configurations.javascript = {
+  {
+    type = 'node2',
+    request = 'launch',
+    program = '${workspaceFolder}/${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+}
 -- C++ apdater
 local dap = require('dap')
 dap.adapters.lldb = {
