37b5c778b5da5072c2e7589fc0d3af5cb738e78d
Igor <igorlfs@ufmg.br>
2022-08-17 06:37:56 -0300

    [nvim] lsp: lua (nvim) server

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -97,6 +97,31 @@ require("rust-tools").setup({
         }
     }
 })
+lspconfig.sumneko_lua.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    handlers = handlers,
+}
 
 -- General config
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
