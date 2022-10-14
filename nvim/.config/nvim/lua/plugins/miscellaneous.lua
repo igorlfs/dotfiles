f666295d0cc0041a53ad456ab96055848cf6139f
Igor <igorlfs@ufmg.br>
2022-10-14 13:20:47 -0300

    [nvim] new sources for null-ls

--- nvim/.config/nvim/lua/plugins/miscellaneous.lua
+++ nvim/.config/nvim/lua/plugins/miscellaneous.lua
@@ -44,6 +44,7 @@ local null_ls = require("null-ls")
 local augroup = ag("LspFormatting", {})
 null_ls.setup({
     on_attach = function(client, bufnr)
+        -- Autoformat on save
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             au("BufWritePre", {
@@ -58,6 +59,8 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.diagnostics.todo_comments,
     },
 })
 
