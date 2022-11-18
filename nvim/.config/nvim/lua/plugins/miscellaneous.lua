0a1fcf96eb3467df49f6677acd95e4988ba93ed0
Igor <igorlfs@ufmg.br>
2022-11-18 13:24:38 -0300

    [nvim] treesitter: remove comment parser
    rationale:
        - it's slow in largers files,
        - it bugs plugins like nvim-navic and indent_blankline, which stop
          working when in a comment

--- nvim/.config/nvim/lua/plugins/miscellaneous.lua
+++ nvim/.config/nvim/lua/plugins/miscellaneous.lua
@@ -21,7 +21,7 @@ require("neorg").setup({
 })
 
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "cpp", "comment", "make", "lua", "python", "vim", "norg" },
+    ensure_installed = { "c", "cpp", "make", "lua", "python", "vim", "norg" },
     highlight = {
         enable = true,
     },
