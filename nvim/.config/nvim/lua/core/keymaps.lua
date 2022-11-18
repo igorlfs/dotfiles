bb4a64599551f2dca9898e5f45a1b92db2dcd6eb
Igor <igorlfs@ufmg.br>
2022-11-18 13:22:48 -0300

    [nvim] maps: open current buffer in new tab, prefer <cmd>

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -16,7 +16,7 @@ keymap("n", "<C-s>", "<cmd>write<CR>")
 keymap("t", "<Esc>", "<C-\\><C-n>")
 
 -- Toggle spell checking
-keymap("n", "<leader>s", ":setlocal spell!<CR>", { silent = true })
+keymap("n", "<leader>s", "<cmd>setlocal spell!<CR>")
 
 -- Tabs
 -- Movement
@@ -27,3 +27,5 @@ keymap("n", "<A-0>", "<cmd>tablast<CR>")
 -- Switch
 keymap("n", "<A-tab>", "<cmd>tabnext<CR>")
 keymap("n", "<S-tab>", "<cmd>tabprevious<CR>")
+-- New tab
+keymap("n", "<A-t>", "<cmd>tab split<CR>")
