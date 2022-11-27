8ff32c3904a80c3821eeecef0fc84e7a0bd15d34
Igor <igorlfs@ufmg.br>
2022-11-27 00:17:37 -0300

    [nvim] telescope: show hidden and mapping for spell suggest
    Requires ignoring ".git" folder explicitly

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,18 @@ tl.setup({
     tl.load_extension("fzy_native"),
     tl.load_extension("noice"),
     tl.load_extension("session-lens"),
+    pickers = {
+        find_files = {
+            hidden = true,
+            file_ignore_patterns = { "^.git" },
+        },
+        live_grep = {
+            additional_args = function()
+                return { "--hidden" }
+            end,
+            file_ignore_patterns = { "^.git" },
+        },
+    },
 })
 
 local keymap = vim.keymap.set
@@ -15,3 +27,5 @@ keymap("n", "<leader>fb", builtin.buffers)
 
 local session = require("session-lens")
 keymap("n", "<leader>fs", session.search_session)
+
+keymap("n", "z=", builtin.spell_suggest)
