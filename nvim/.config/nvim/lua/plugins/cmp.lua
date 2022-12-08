10d751e61015c8437ee49a04e67f7a30cf6ffd87
Igor <igorlfs@ufmg.br>
2022-12-07 23:44:31 -0300

    [nvim] noice: quality of life improvements
    - classic command line
    - show "recording" messages
    - hide "written" messages
    - scroll documentation/signature help

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -13,7 +13,7 @@ cmp.setup({
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm({
--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,18 +1,48 @@
 require("noice").setup({
     messages = {
-        view_search = false, -- disable virtualtext  with search count
+        view_search = false, -- disable view for search count messages
     },
     lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
-            -- override the default lsp markdown formatter with Noice
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            -- override the lsp markdown formatter with Noice
             ["vim.lsp.util.stylize_markdown"] = true,
-            -- override cmp documentation with Noice (needs the other options to work)
             ["cmp.entry.get_documentation"] = true,
         },
     },
     presets = {
         lsp_doc_border = true, -- add a border to hover docs and signature help
     },
+    -- "Classic" command line
+    cmdline = {
+        view = "cmdline",
+    },
+    routes = {
+        -- Show "recording" messages
+        {
+            view = "notify",
+            filter = { event = "msg_showmode" },
+        },
+        -- Hide "written" messages
+        {
+            filter = {
+                event = "msg_show",
+                kind = "",
+                find = "written",
+            },
+            opts = { skip = true },
+        },
+    },
 })
+
+vim.keymap.set({ "n", "i" }, "<C-d>", function()
+    if not require("noice.lsp").scroll(4) then
+        return "<C-d>"
+    end
+end, { silent = true, expr = true })
+
+vim.keymap.set({ "n", "i" }, "<C-u>", function()
+    if not require("noice.lsp").scroll(-4) then
+        return "<C-u>"
+    end
+end, { silent = true, expr = true })
