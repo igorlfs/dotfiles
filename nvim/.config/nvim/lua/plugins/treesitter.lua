9e8b14beef42034ca1da21a3a85b3fd1931f0f50
Igor <igorlfs@ufmg.br>
2022-12-15 22:49:02 -0300

    nvim(treesitter): add more languages to ensure_installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,25 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "cpp", "make", "lua", "python", "vim", "norg", "toml", "java" },
+    ensure_installed = {
+        "c",
+        "cpp",
+        "make",
+        "lua",
+        "python",
+        "vim",
+        "norg",
+        "toml",
+        "java",
+        "gitcommit",
+        "yaml",
+        "regex",
+        "bash",
+        "markdown",
+        "markdown_inline",
+    },
     auto_install = true,
     highlight = {
         enable = true,
-        disable = function(lang, buf)
+        disable = function(_, buf)
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
