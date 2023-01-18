f27577af5fb66cf01f9a0f9aa2d6b7005bf6ab63
Igor <igorlfs@ufmg.br>
2023-01-18 19:53:58 -0300

    sway(bar): move to i3blocks

---
+++ scripts/scripts/i3battery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BAT_CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
+BAT_STATUS=$(cat /sys/class/power_supply/BAT0/status)
+
+case ${BAT_STATUS} in
+    Charging) BAT_STATUS="+" ;;
+    Discharging) BAT_STATUS="-" ;;
+    Full) BAT_CAPACITY="Full" ;;
+    *) BAT_STATUS="?"
+esac
+
+if [ ! ${BAT_CAPACITY} = "Full" ]; then
+    BAT_CAPACITY="${BAT_CAPACITY}%"
+fi
+
+echo "${BAT_STATUS}${BAT_CAPACITY}"
