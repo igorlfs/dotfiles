27bc45370efd9959c2e3d7579710aa9ee8059c0e
Igor <igorlfs@ufmg.br>
2023-02-20 13:55:07 -0300

    nvim: don't extend capabilities that nvim-cmp-lsp already sets

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -78,8 +78,6 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 M.capabilities = require("cmp_nvim_lsp").default_capabilities()
--- Enable additional completion for HTML/JSON/CSS
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- Add additional capabilities supported by UFO
 M.capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
