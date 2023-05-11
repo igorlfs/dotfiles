3776182001774954f97bf536e625bc72de3613b3
Igor <igorlfs@ufmg.br>
2023-05-11 00:23:46 -0300

    nvim(Treesitter): add some text objects

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -51,6 +51,16 @@ treesitter.setup({
         disable = { "python" },
     },
     textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
         lsp_interop = {
             enable = true,
             border = "rounded",
