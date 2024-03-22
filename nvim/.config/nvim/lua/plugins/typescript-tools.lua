4f3b5f741f510868521c3cd12db6442857bfba5d
Igor <igorlfs@ufmg.br>
2024-03-22 17:03:26 -0300

    nvim(typescript-tools): lazy load for react

--- nvim/.config/nvim/lua/plugins/typescript-tools.lua
+++ nvim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -1,6 +1,6 @@
 return {
     "pmizio/typescript-tools.nvim",
-    ft = { "typescript", "javascript" },
+    ft = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "neovim/nvim-lspconfig",
