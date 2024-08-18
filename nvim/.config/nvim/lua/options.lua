41d334ef7a57d787dd2d12ef1b25cac9dd531c6a
Igor <igorlfs@ufmg.br>
2024-08-18 16:08:50 -0300

    nvim!: remove molten-nvim
    Notebook setup is too complicated and isn't a great experience.
    
    When it comes to editing jupyter files, the setup with jupytext.nvim is
    rather sluggish with large notebooks. Even if that weren't an issue,
    it's overall awkward to use. For instance, the display of images and
    even pandas' dataframes is lacking and the interaction with the kernel
    also gets really slow. When the need to edit notebooks comes again, I'll
    give this setup a second chance but I'm not very optimistic.
    
    The non-notebook experience suffers with some of these issues too. But
    I barely use it, so currently there's no need to keep molten around.

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -68,7 +68,7 @@ o.shiftround = true -- Round indent to multiple of 'shiftwidth'
 
 -- Providers
 
-g.python3_host_prog = config .. "/.venv/bin/python"
+g.loaded_python3_provider = 0
 g.loaded_node_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
