c2e3c35426e682253dc03108254dc47b5fdf7ca5
Igor <igorlfs@ufmg.br>
2024-12-19 18:04:16 -0300

    nvim(conform): jq - indent with 4 spaces
    Since the main use case is to format JSON embedded into HTTP files, it
    makes sense format with a non-cringe number of spaces (which is mostly
    consistent with what I use everywhere)

--- nvim/.config/nvim/lua/plugins/bare/conform.lua
+++ nvim/.config/nvim/lua/plugins/bare/conform.lua
@@ -11,6 +11,9 @@ return {
                     },
                 },
             },
+            jq = {
+                prepend_args = { "--indent", "4" },
+            },
         },
         formatters_by_ft = {
             lua = { "stylua" },
