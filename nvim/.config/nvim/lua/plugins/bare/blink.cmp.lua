6731b09ee52701fce6c0ef71994dea854e5a0155
Igor <igorlfs@ufmg.br>
2025-01-20 00:52:21 -0300

    nvim(blink): use enter preset
    As it shares some my keymappings with my setup

--- nvim/.config/nvim/lua/plugins/bare/blink.cmp.lua
+++ nvim/.config/nvim/lua/plugins/bare/blink.cmp.lua
@@ -21,8 +21,7 @@ return {
             },
         },
         keymap = {
-            preset = "none",
-            ["<CR>"] = { "accept", "fallback" },
+            preset = "enter",
             ["<Tab>"] = {
                 "select_next",
                 "snippet_forward",
@@ -33,8 +32,6 @@ return {
                 "snippet_backward",
                 "fallback",
             },
-            ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-            ["<C-e>"] = { "hide" },
             ["<C-u>"] = { "scroll_documentation_up", "fallback" },
             ["<C-d>"] = { "scroll_documentation_down", "fallback" },
         },
