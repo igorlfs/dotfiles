8acb5bc3e2945e73eeba41270e0de0c9a320d891
Igor <igorlfs@ufmg.br>
2025-02-23 19:57:17 -0300

    nvim(noice): remove keymaps I wasn't using

--- nvim/.config/nvim/lua/plugins/bare/noice.lua
+++ nvim/.config/nvim/lua/plugins/bare/noice.lua
@@ -24,30 +24,4 @@ return {
             lsp_doc_border = true, -- add a border to hover docs and signature help
         },
     },
-    keys = {
-        {
-            "<c-d>",
-            function()
-                if not require("noice.lsp").scroll(4) then
-                    return "<c-d>"
-                end
-            end,
-            silent = true,
-            expr = true,
-            desc = "Scroll forward",
-            mode = { "i", "n", "s" },
-        },
-        {
-            "<c-u>",
-            function()
-                if not require("noice.lsp").scroll(-4) then
-                    return "<c-u>"
-                end
-            end,
-            silent = true,
-            expr = true,
-            desc = "Scroll backward",
-            mode = { "i", "n", "s" },
-        },
-    },
 }
