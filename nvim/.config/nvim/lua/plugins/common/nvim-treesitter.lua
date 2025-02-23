c471fc06c645ab0a39ae8f8bdcd3a450e858378c
Igor <igorlfs@ufmg.br>
2025-02-23 19:57:37 -0300

    nvim: do not lazy load treesitter

--- nvim/.config/nvim/lua/plugins/common/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/common/nvim-treesitter.lua
@@ -2,7 +2,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
-    event = "VeryLazy",
     config = function()
         ---@diagnostic disable-next-line: missing-fields
         require("nvim-treesitter.configs").setup({
