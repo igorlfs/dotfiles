21ddefa6170f1f7600119ec62fe29a09254b113d
Igor <igorlfs@ufmg.br>
2025-03-07 22:53:19 -0300

    nvim: statuscol is back in the game
    Now without the fold column

---
+++ nvim/.config/nvim/lua/plugins/bare/statuscol.lua
@@ -0,0 +1,25 @@
+return {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+            relculright = true,
+            segments = {
+                { sign = { name = { "Dap" }, namespace = { "diagnostic" } } },
+                { text = { builtin.lnumfunc, " " } },
+                { sign = { namespace = { "gitsigns" }, wrap = true } },
+            },
+            ft_ignore = {
+                "NvimTree",
+                "NeogitStatus",
+                "NeogitPopup",
+                "NeogitCommitMessage",
+                "NeogitCommitView",
+                "dap-repl",
+            },
+            bt_ignore = {
+                "terminal",
+            },
+        })
+    end,
+}
