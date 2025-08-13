f9cffc6a1d37b4400b2a728367a7922d97895d02
Igor <igorlfs@ufmg.br>
2025-08-13 00:09:21 -0300

    nvim(queries): format

--- nvim/.config/nvim/queries/python/injections.scm
+++ nvim/.config/nvim/queries/python/injections.scm
@@ -2,5 +2,5 @@
 
 ; shout out to https://github.com/milanglacier/nvim/blob/master/after/queries/python/injections.scm
 (((string_content) @sql
-                   (#lua-match? @sql "^%s*--sql")) @injection.content
- (#set! injection.language "sql"))
+  (#lua-match? @sql "^%s*--sql")) @injection.content
+  (#set! injection.language "sql"))
