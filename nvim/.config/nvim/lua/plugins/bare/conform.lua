afdff6d0fa498bd166bacda8479053fed0c07fa3
Igor <igorlfs@ufmg.br>
2025-08-19 18:50:21 -0300

    nvim: .sqruff is ini, format yaml via lsp

--- nvim/.config/nvim/lua/plugins/bare/conform.lua
+++ nvim/.config/nvim/lua/plugins/bare/conform.lua
@@ -20,7 +20,6 @@ return {
         formatters_by_ft = {
             lua = { "stylua" },
             markdown = { "injected", "markdownlint" },
-            yaml = { "prettier" },
             typescript = { "biome-check", "prettier", stop_after_first = true },
             javascript = { "biome-check", "prettier", stop_after_first = true },
             json = { "biome" },
