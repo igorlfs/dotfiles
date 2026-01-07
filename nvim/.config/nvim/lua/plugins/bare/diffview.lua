2ff1ae29fe5b372deafe6c3b325c219b44bad595
Igor <igorlfs@ufmg.br>
2026-01-07 01:16:24 -0300

    nvim(diffview): disable `file_panel` at the bottom
    It's been like a year and I'm still on the fence about this? smh my head
    
    The major issue with the bottom is that when many files are changed,
    it's hard to keep track of their structure, since 10 lines is only so
    much

--- nvim/.config/nvim/lua/plugins/bare/diffview.lua
+++ nvim/.config/nvim/lua/plugins/bare/diffview.lua
@@ -5,29 +5,17 @@ return {
         { "<leader>vo", "<CMD>DiffviewOpen<CR>", desc = "Diff Head" },
         { "<leader>vc", "<CMD>DiffviewFileHistory %<CR>", desc = "Diff Current File" },
     },
-    config = function()
-        local actions = require("diffview.actions")
-
-        require("diffview").setup({
+    opts = {
+        keymaps = {
+            view = {
+                { "n", "q", "<CMD>DiffviewClose<CR>" },
+            },
             file_panel = {
-                win_config = {
-                    position = "bottom",
-                    height = 10,
-                },
+                { "n", "q", "<CMD>DiffviewClose<CR>" },
             },
-            keymaps = {
-                view = {
-                    { "n", "q", actions.close, { desc = "DiffviewClose" } },
-                },
-                file_panel = {
-                    -- actions.close closes only the file_panel window
-                    { "n", "q", "<CMD>DiffviewClose<CR>", { desc = "DiffviewClose" } },
-                },
-                file_history_panel = {
-                    -- similarly, actions.close closes only the file_history_panel window
-                    { "n", "q", "<CMD>DiffviewClose<CR>", { desc = "DiffviewClose" } },
-                },
+            file_history_panel = {
+                { "n", "q", "<CMD>DiffviewClose<CR>" },
             },
-        })
-    end,
+        },
+    },
 }
