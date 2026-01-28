f1bd89b3c142c5b81b0d19434ddcb58de9ef7b4c
Igor <igorlfs@ufmg.br>
2026-01-27 21:37:07 -0300

    nvim(possession): do not delete hidden buffers
    This has shrunk to a much lesser issue, now that fzf-lua ignores
    vim.ui.select with the "hide" profile.
    
    Used to be the case vim.ui.select would be used to pick the session and
    would to be cleared straightaway

--- nvim/.config/nvim/lua/plugins/bare/possession.lua
+++ nvim/.config/nvim/lua/plugins/bare/possession.lua
@@ -5,12 +5,6 @@ return {
         autosave = {
             current = true,
         },
-        plugins = {
-            -- Force deleting fzf-lua's "hidden" terminal (last search)
-            delete_hidden_buffers = {
-                force = function(buf) return vim.bo[buf].buftype == "terminal" end,
-            },
-        },
     },
     keys = {
         { "<leader>fs", "<CMD>PossessionPick<CR>", desc = "Find Sessions" },
