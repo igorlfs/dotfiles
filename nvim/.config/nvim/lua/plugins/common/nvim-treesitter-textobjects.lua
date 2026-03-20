782a9f31c53521faa47ab0bba5ac40c44b8d3c60
Igor <igorlfs@ufmg.br>
2026-03-20 00:49:55 -0300

    nvim: enable selection using nvim-treesitter-textobjects

--- nvim/.config/nvim/lua/plugins/common/nvim-treesitter-textobjects.lua
+++ nvim/.config/nvim/lua/plugins/common/nvim-treesitter-textobjects.lua
@@ -2,10 +2,17 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
     branch = "main",
     keys = function()
-        local swap = require("nvim-treesitter-textobjects.swap")
+        local ts_swap = require("nvim-treesitter-textobjects.swap")
+        local ts_select = require("nvim-treesitter-textobjects.select").select_textobject
         return {
-            { "<C-.>", function() swap.swap_next("@parameter.inner") end },
-            { "<C-,>", function() swap.swap_previous("@parameter.inner") end },
+            -- stylua: ignore start
+            { "<C-.>", function() ts_swap.swap_next("@parameter.inner") end },
+            { "<C-,>", function() ts_swap.swap_previous("@parameter.inner") end },
+            { "af", function() ts_select("@function.outer") end, mode = { "x", "o" } },
+            { "if", function() ts_select("@function.inner") end, mode = { "x", "o" } },
+            { "aa", function() ts_select("@parameter.outer") end, mode = { "x", "o" } },
+            { "ia", function() ts_select("@parameter.inner") end, mode = { "x", "o" } },
+            -- stylua: ignore end
         }
     end,
 }
