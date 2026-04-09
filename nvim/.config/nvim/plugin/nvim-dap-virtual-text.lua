dede2bb4e67eb953ee283566cedd3045e03a475e
Igor <igorlfs@ufmg.br>
2026-04-09 18:24:37 -0300

    nvim: drop nvim-dap-virtual-text
    Move to nvim-dap-view's virtual-text

--- nvim/.config/nvim/plugin/nvim-dap-virtual-text.lua
+++
@@ -1,9 +0,0 @@
-local util = require("igorlfs.util")
-
-vim.pack.add({
-    { src = util.gh("theHamsta/nvim-dap-virtual-text") },
-})
-
-require("nvim-dap-virtual-text").setup({ enabled = false })
-
-util.keymap("<A-v>", "<CMD>DapVirtualTextToggle<CR>", "Toggle DAP Virtual Text")
