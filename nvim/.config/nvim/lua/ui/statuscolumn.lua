62a027cb6beaf6ad7557992a0b8eb890beb695d4
Igor <igorlfs@ufmg.br>
2026-04-11 13:07:47 -0300

    nvim(statuscolumn): built-in gitsigns statuscolumn
    It just works

--- nvim/.config/nvim/lua/ui/statuscolumn.lua
+++ nvim/.config/nvim/lua/ui/statuscolumn.lua
@@ -2,27 +2,12 @@ local api = vim.api
 
 local M = {}
 
----@type string[]
-local _ns = {}
----@param id integer
-local function ns_name(id)
-    if not _ns[id] then
-        for name, nsid in pairs(api.nvim_get_namespaces()) do
-            _ns[nsid] = name
-        end
-    end
-    return _ns[id] or ""
-end
-
 ---@param lnum integer
----@return [vim.api.keyset.extmark_details, vim.api.keyset.extmark_details]
 local function get_signs(lnum)
     local signs = api.nvim_buf_get_extmarks(0, -1, { lnum - 1, 0 }, { lnum - 1, -1 }, { details = true, type = "sign" })
 
     ---@type vim.api.keyset.extmark_details?
     local dap
-    ---@type vim.api.keyset.extmark_details?
-    local git
     for _, mark in ipairs(signs) do
         local details = mark[4]
         if details and details.sign_text then
@@ -30,25 +15,16 @@ local function get_signs(lnum)
                 if not dap or details.priority > dap.priority then
                     dap = details
                 end
-            elseif ns_name(details.ns_id):find("gitsigns") then
-                if not git or details.priority > git.priority then
-                    git = details
-                end
             end
         end
     end
 
-    return { dap, git }
+    return dap
 end
 
 ---@param sign vim.api.keyset.extmark_details?
 local function render_dap(sign)
     return sign and "%#" .. sign.sign_name .. "#" .. sign.sign_text .. "%*" or "  "
-end
-
----@param sign vim.api.keyset.extmark_details?
-local function render_git(sign)
-    return sign and "%#" .. sign.sign_hl_group .. "#" .. sign.sign_text .. "%*" or "  "
 end
 
 local function number_line()
@@ -68,11 +44,15 @@ M.render = function()
         return number_line()
     end
 
-    local signs = get_signs(vim.v.lnum)
+    local dap_signs = get_signs(vim.v.lnum)
 
-    local dap = vim.v.virtnum == 0 and render_dap(signs[1]) or "  "
+    local dap = vim.v.virtnum == 0 and render_dap(dap_signs) or "  "
 
-    local bare = number_line() .. render_git(signs[2])
+    local bare = number_line()
+
+    if package.loaded["gitsigns"] then
+        bare = bare .. require("gitsigns").statuscolumn()
+    end
 
     if vim.bo[0].modifiable then
         return dap .. bare
