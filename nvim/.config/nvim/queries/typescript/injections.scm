812e47ae65826a5392286644454fa1d0828a0d54
Igor <igorlfs@ufmg.br>
2026-05-15 19:52:30 -0300

    nvim(queries): typescript fancy sql injection

--- nvim/.config/nvim/queries/typescript/injections.scm
+++ nvim/.config/nvim/queries/typescript/injections.scm
@@ -5,3 +5,14 @@
     (string_fragment) @injection.content
     (#set! injection.combined))
   (#gsub! @injection.language "/%*%s*([%w%p]+)%s*%*/" "%1"))
+
+; foo.sql<type>`...`
+(call_expression
+  function: (non_null_expression
+    (instantiation_expression
+      (member_expression
+        property: (property_identifier) @injection.language)))
+  arguments: (template_string) @injection.content
+  (#eq? @injection.language "sql")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children))
