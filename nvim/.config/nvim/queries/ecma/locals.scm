3a8d614de5dd8a123f9be9f5fc971535ff06bafc
Igor <igorlfs@ufmg.br>
2026-05-20 00:03:31 -0300

    nvim(injections): locals reference for js embedded into svelte (markup)

--- nvim/.config/nvim/queries/ecma/locals.scm
+++ nvim/.config/nvim/queries/ecma/locals.scm
@@ -8,3 +8,13 @@
 (variable_declarator
   (array_pattern
     (identifier) @local.definition.var))
+
+; (reference) Svelte injections
+; These are not really useful, the mostly contain gibberish when inspecting with virtual text
+; ($inspect is the prefered way of debugging)
+; {@const foo = ...}
+; (assignment_expression
+;   (identifier) @local.definition)
+; {each ... as foo, bar}
+; (sequence_expression
+;   (identifier) @local.definition)
