9c6ef2737bd3d1bc5beb72d7a795c6191d637a5b
Igor <igorlfs@ufmg.br>
2026-07-06 11:43:10 -0300

    hyprland: format

--- hyprland/.config/hypr/window-rules.lua
+++ hyprland/.config/hypr/window-rules.lua
@@ -3,29 +3,29 @@
 hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
 hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
 hl.window_rule({
-	name = "no-gaps-wtv1",
-	match = { float = false, workspace = "w[tv1]" },
-	border_size = 0,
-	rounding = 0,
+    name = "no-gaps-wtv1",
+    match = { float = false, workspace = "w[tv1]" },
+    border_size = 0,
+    rounding = 0,
 })
 hl.window_rule({
-	name = "no-gaps-f1",
-	match = { float = false, workspace = "f[1]" },
-	border_size = 0,
-	rounding = 0,
-})
-
-hl.window_rule({
-	name = "float-file-dialogs",
-	-- https://github.com/google/re2/wiki/Syntax
-	match = { class = "chromium", title = "(Open|Save) Files?" },
-	float = true,
-	center = true,
-	size = { 900, 550 },
+    name = "no-gaps-f1",
+    match = { float = false, workspace = "f[1]" },
+    border_size = 0,
+    rounding = 0,
 })
 
 hl.window_rule({
-	name = "no-screen-share-whatsapp",
-	match = { class = "firefox", title = [[\(\d+\) WhatsApp — Mozilla Firefox]] },
-	no_screen_share = true,
+    name = "float-file-dialogs",
+    -- https://github.com/google/re2/wiki/Syntax
+    match = { class = "chromium", title = "(Open|Save) Files?" },
+    float = true,
+    center = true,
+    size = { 900, 550 },
+})
+
+hl.window_rule({
+    name = "no-screen-share-whatsapp",
+    match = { class = "firefox", title = [[\(\d+\) WhatsApp — Mozilla Firefox]] },
+    no_screen_share = true,
 })
