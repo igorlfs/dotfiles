a46b0f88a0c02a84f1e7087b92f53e98a0d311fc
Igor <igorlfs@ufmg.br>
2026-09-08 20:23:09 -0300

    nvim: format

--- nvim/.config/nvim/plugin/nvim-dap.lua
+++ nvim/.config/nvim/plugin/nvim-dap.lua
@@ -14,24 +14,28 @@ require("igorlfs.dap.debugpy")
 require("igorlfs.dap.js-debug-adapter")
 
 --- Keymaps
--- stylua: ignore start
 util.keymap("[f", dap.up, "DAP Up")
 util.keymap("]f", dap.down, "DAP Down")
 util.keymap("<F5>", dap.continue, "DAP Continue")
 util.keymap("<F7>", dap.focus_frame, "DAP Focus Frame")
 util.keymap("<F8>", function()
-    vim.ui.input({ prompt = "Log point message: " }, function(input) dap.set_breakpoint(nil, nil, input) end)
+    vim.ui.input({ prompt = "Log point message: " }, function(input)
+        dap.set_breakpoint(nil, nil, input)
+    end)
 end, "Toggle Logpoint")
 util.keymap("<F9>", dap.toggle_breakpoint, "Toggle Breakpoint")
 util.keymap("<F10>", dap.step_over, "Step Over")
 util.keymap("<F11>", dap.step_into, "Step Into")
-util.keymap("<F16>", function() dap.terminate({ hierarchy = true }) end, "DAP Terminate")
+util.keymap("<F16>", function()
+    dap.terminate({ hierarchy = true })
+end, "DAP Terminate")
 util.keymap("<F18>", dap.run_to_cursor, "Run to Cursor")
 util.keymap("<F21>", function()
-    vim.ui.input({ prompt = "Breakpoint condition: " }, function(input) dap.set_breakpoint(input) end)
+    vim.ui.input({ prompt = "Breakpoint condition: " }, function(input)
+        dap.set_breakpoint(input)
+    end)
 end, "Conditional Breakpoint")
 util.keymap("<F23>", dap.step_out, "Step Out")
--- stylua: ignore end
 
 -- Signs
 for _, group in pairs({
