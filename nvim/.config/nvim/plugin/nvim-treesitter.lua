e6b32a6f0f58fe2372fa37d61e66126d575e58e3
Igor <igorlfs@ufmg.br>
2026-09-14 23:17:58 -0300

    nvim(treesitter): cleanup auto update hook

--- nvim/.config/nvim/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/plugin/nvim-treesitter.lua
@@ -1,13 +1,12 @@
+local api = vim.api
+
 local util = require("igorlfs.util")
 
-vim.api.nvim_create_autocmd("PackChanged", {
+api.nvim_create_autocmd("PackChanged", {
     callback = function(ev)
         local name, kind = ev.data.spec.name, ev.data.kind
 
         if name == "nvim-treesitter" and kind == "update" then
-            if not ev.data.active then
-                vim.cmd.packadd("nvim-treesitter")
-            end
             vim.cmd("TSUpdate")
         end
     end,
